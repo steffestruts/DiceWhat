303678520e8423f218b193c99527cb430b396106
Stefan Sjöberg <bird.nerd@outlook.com>
2024-12-14 12:01:43 +0100

    Add project files.

---
+++ MainApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+List<string> lista = [];
+
+Random random = new();
+
+Console.WriteLine("Skriv in saker du vill ha gjort och sedan slumpar jag! (För avsluta, skriv: Stopp)\n");
+
+while (true)
+{
+    string? innerinput = Console.ReadLine();
+
+    if (innerinput == "stopp" || innerinput == null)
+    {
+        Console.Clear();
+        break;
+    }
+    else
+    {
+        lista.Add(innerinput!);
+        Console.WriteLine($"Inmatningen: {innerinput} är inlagd.\n");
+    }
+}
+
+// Hämtat från ChatGPT hur random fungerar
+int index = random.Next(lista.Count);
+// Slumpen väljs av inmatade alternativ - bugg om man skriver in "stopp" som ett alternativ i början
+string slumpen = lista[index];
+
+Console.WriteLine("-------------- Jag har valt ett slumpmässigt alternativ --------------");
+
+Console.WriteLine(slumpen);
+
+Console.ReadKey();
